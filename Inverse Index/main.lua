ec8c5efa87cec8ce104eabb1579ef732488091ab
Lukas Gruber <lgruber.mmt-b2016@fh-salzburg.ac.at>
2018-04-21 01:12:31 +0200

    feat(Aufgabe01): add simple file indexing function

--- Inverse Index/main.lua
+++ Inverse Index/main.lua
@@ -1,8 +1,27 @@
 local fs = require("lfs")
-local resource_path = lfs.currentdir() .. "/Inverse Index/resource"
 
-for file in fs.dir(resource_path) do
+local index = {}
+
+function setupIndex()
+  local resource_path = lfs.currentdir() .. "/Inverse Index/resource"
+  
+  for file in fs.dir(resource_path) do
   if file ~= "." and file ~= ".." then
-    print(file)
+    for line in io.lines(resource_path .. "/" .. file) do
+      for i in string.gmatch(line, "%S+") do
+        local word = string.match(i, "[a-zA-Z0-9]+") -- filter chars and digits 
+        if word and #word > 1 then -- if not nil and length > 1
+          word = string.lower(word)
+          if not index[word] then
+            index[word] = {}
+          end
+
+          table.insert(index[word], file)
+        end
+      end
+    end
   end
 end
+end
+
+setupIndex()
