b1eee267bb049c0b3c33393ee4260619928d3bd1
Lukas Gruber <lgruber.mmt-b2016@fh-salzburg.ac.at>
2018-04-21 02:21:45 +0200

    feat(Aufgabe01): add posting list intersection

--- Inverse Index/main.lua
+++ Inverse Index/main.lua
@@ -1,27 +1,90 @@
+
 local fs = require("lfs")
 
-local index = {}
+local subdir = "Inverse Index"
+local path = fs.currentdir() .. "/" .. subdir
 
-function setupIndex()
-  local resource_path = lfs.currentdir() .. "/Inverse Index/resource"
-  
+local function setupIndex(index)
+  local resource_path = path .. "/resource"
+
   for file in fs.dir(resource_path) do
-  if file ~= "." and file ~= ".." then
-    for line in io.lines(resource_path .. "/" .. file) do
-      for i in string.gmatch(line, "%S+") do
-        local word = string.match(i, "[a-zA-Z0-9]+") -- filter chars and digits 
-        if word and #word > 1 then -- if not nil and length > 1
-          word = string.lower(word)
-          if not index[word] then
-            index[word] = {}
-          end
+    if file ~= "." and file ~= ".." then
+      for line in io.lines(resource_path .. "/" .. file) do
+        for i in string.gmatch(line, "%S+") do
+          local word = string.match(i, "[a-zA-Z0-9]+") -- filter chars and digits 
+          if word and #word > 1 then -- if not nil and length > 1
+            word = string.lower(word)
+            if not index[word] then
+              index[word] = {}
+            end
 
-          table.insert(index[word], file)
+            table.insert(index[word], file)
+          end
         end
       end
     end
   end
 end
+
+local function find(a, t)
+  for _,a_ in ipairs(t) do 
+    if a_== a then 
+      return true 
+    end 
+  end
 end
 
-setupIndex()
+local function intersect(a, b)
+  local ret = {}
+	for _,b_ in ipairs(b) do
+		if find(b_,a) then table.insert(ret, b_) end
+	end
+	return ret
+end
+
+local function filterIndex(index, query_terms)
+  local postings = {}
+  
+  for _, term in pairs(query_terms) do
+    if index[term] then
+      table.insert(postings, index[term])
+    end
+  end
+  
+  local result
+  
+  for i in pairs(postings) do
+    if not result then
+      result = postings[i]
+    else 
+      result = intersect(result, postings[i])
+    end
+  end
+  
+  return result and result or {}
+end
+
+local index = {}
+setupIndex(index)
+
+local query_terms = {}
+
+while true do
+  print("Enter query, empty to quit:")
+
+  local term = io.read("*l")
+  if #term == 0 then
+    break
+  end
+  
+  table.insert(query_terms, string.lower(term))
+  local result = filterIndex(index, query_terms)
+  
+  print(#result .. " results:")
+  
+  for _, resource in pairs(result) do
+    print(resource)
+  end
+end
+
+print("goodbye")
