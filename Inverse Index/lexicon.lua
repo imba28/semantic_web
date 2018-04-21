952029b85b6fafcdee54a619b93a4207a8358b2b
Lukas Gruber <lgruber.mmt-b2016@fh-salzburg.ac.at>
2018-04-21 02:40:14 +0200

    refactor(Aufgabe01): extract lexicon methods

---
+++ Inverse Index/lexicon.lua
@@ -0,0 +1,51 @@
+local fs = require("lfs")
+local intersect = require(subdir .. ".operations").intersect
+
+local function setup(index, path)
+  for file in fs.dir(path) do
+    if file ~= "." and file ~= ".." then
+      for line in io.lines(path .. "/" .. file) do
+        for i in string.gmatch(line, "%S+") do
+          local word = string.match(i, "[a-zA-Z0-9]+") -- filter chars and digits 
+          if word and #word > 1 then -- if not nil and length > 1
+            word = string.lower(word)
+            if not index[word] then
+              index[word] = {}
+            end
+
+            table.insert(index[word], file)
+          end
+        end
+      end
+    end
+  end
+end
+
+local function filter(index, query_terms)
+  local postings = {}
+  
+  for _, term in pairs(query_terms) do
+    if index[term] then
+      table.insert(postings, index[term])
+    else 
+      table.insert(postings, {})
+    end
+  end
+  
+  local result
+  
+  for i in pairs(postings) do
+    if not result then
+      result = postings[i]
+    else 
+      result = intersect(result, postings[i])
+    end
+  end
+  
+  return result and result or {}
+end
+
+return {
+  setup = setup,
+  filter = filter
+}
--- Inverse Index/main.lua
+++ Inverse Index/main.lua
@@ -1,61 +1,15 @@
 
-local fs = require("lfs")
+subdir = "Inverse Index"
 
-local subdir = "Inverse Index"
+local fs = require("lfs")
 local path = fs.currentdir() .. "/" .. subdir
 
-local intersect = require(subdir .. ".operations").intersect
-
-local function setupIndex(index)
-  local resource_path = path .. "/resource"
-
-  for file in fs.dir(resource_path) do
-    if file ~= "." and file ~= ".." then
-      for line in io.lines(resource_path .. "/" .. file) do
-        for i in string.gmatch(line, "%S+") do
-          local word = string.match(i, "[a-zA-Z0-9]+") -- filter chars and digits 
-          if word and #word > 1 then -- if not nil and length > 1
-            word = string.lower(word)
-            if not index[word] then
-              index[word] = {}
-            end
-
-            table.insert(index[word], file)
-          end
-        end
-      end
-    end
-  end
-end
-
-local function filterIndex(index, query_terms)
-  local postings = {}
-  
-  for _, term in pairs(query_terms) do
-    if index[term] then
-      table.insert(postings, index[term])
-    else 
-      table.insert(postings, {})
-    end
-  end
-  
-  local result
-  
-  for i in pairs(postings) do
-    if not result then
-      result = postings[i]
-    else 
-      result = intersect(result, postings[i])
-    end
-  end
-  
-  return result and result or {}
-end
+local lexicon = require(subdir .. ".lexicon")
 
 local index = {}
-setupIndex(index)
-
 local query_terms = {}
+
+lexicon.setup(index, path .. "/resource")
 
 while true do
   print("Enter query, empty to quit:")
@@ -64,9 +18,9 @@ while true do
   if #term == 0 then
     break
   end
-  
+
   table.insert(query_terms, string.lower(term))
-  local result = filterIndex(index, query_terms)
+  local result = lexicon.filter(index, query_terms)
   
   print(#result .. " results:")
   
