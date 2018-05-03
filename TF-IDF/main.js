6ec2f911bb753844497713f507cacff12f34d5a2
Lukas Gruber <lgruber.mmt-b2016@fh-salzburg.ac.at>
2018-05-03 20:31:17 +0200

    feat(tf-idf): add input processing

--- TF-IDF/main.js
+++ TF-IDF/main.js
@@ -27,17 +27,46 @@ files.forEach(file => {
     })
 })
 
-filter()
+const documentFrequency = term => Object.keys(index[term].docs).length 
+const inverseDocumentFrequency = term => files.length / documentFrequency(term)
 
-function filter() {
+function calcIDF() {
+    for ([term, info] of Object.entries(index)) {
+        info.idf = inverseDocumentFrequency(term)
+    }
+}
+
+calcIDF()
+askForInput()
+
+function askForInput() {
     rl.question('Enter query, empty to quit: ', input => {
         if (input.length == 0) {
             console.log('Goodbye!')
             process.exit(0)
         }
-        console.log(`${files.length} documents`)
-        console.log('Found x results, showing top 5')
+        //console.log('Found x results, showing top 5')
+        const scores = []
 
-        filter()
+        input.split(/\s+/).forEach(queryTerm => {
+            if (!index[queryTerm]) return 0;
+
+            for (let document in index[queryTerm].docs) {
+                let documentScore = scores.find(item => item.document === document)
+
+                if (!documentScore) {
+                    documentScore = {
+                        document,
+                        score: 0
+                    }
+                    scores.push(documentScore)
+                }
+                documentScore.score += index[queryTerm].docs[document] * index[queryTerm].idf
+            }
+        })
+
+        console.log(scores)
+
+        askForInput()
     })
 }
